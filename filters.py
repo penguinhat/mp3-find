2923c2d1f0ba077bdff625f3bd1e4a08e2369e7e
William Hunt <william.david.hunt@googlemail.com>
2012-06-05 14:43:29 +0100

    Made filters case insensitive

--- filters.py
+++ filters.py
@@ -1,10 +1,10 @@
 import eyeD3
 
 def get_artist(tag,query):
-    return query in tag.getArtist() 
+    return query.lower() in tag.getArtist().lower()
 
 def get_title(tag,query):
-    return query in tag.getTitle() 
+    return query.lower() in tag.getTitle().lower() 
 
 def get_album(tag,query):
-    return query in tag.getAlbum()
+    return query.lower() in tag.getAlbum().lower()
